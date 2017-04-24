c85df6d737e06c88af7ba92f36a84b29549ec503
BlackSharp <makcum31ru@gmail.com>
2017-04-24 23:50:42 +0300

    123

--- TerrorAutoUltiAfterfix/Terror.cs
+++ TerrorAutoUltiAfterfix/Terror.cs
@@ -10,9 +10,9 @@ namespace TerrorAutoUlti
     internal class TerrorAutoUlti
     {
         private static readonly Menu Menu = new Menu("TerrorAutoUlti", "TerrorAutoUlti by Black", true, "npc_dota_hero_terrorblade", true);
-        private static Hero _me;
+        private static Hero me;
         private static System.Collections.Generic.List<Hero> _enemies;
-        private static bool _loaded;
+        private static bool loaded;
         private static void OnLoadEvent(object sender, EventArgs args)
         {
             if (ObjectManager.LocalHero.ClassId != ClassId.CDOTA_Unit_Hero_Terrorblade) return;
@@ -35,13 +35,13 @@ namespace TerrorAutoUlti
         }
         private static void Game_OnUpdate(EventArgs args)
         {
-            if (!_loaded)
+            if (!loaded)
             {
-                _me = ObjectManager.LocalHero;
+                me = ObjectManager.LocalHero;
 
-                if (!Game.IsInGame || Game.IsWatchingGame || _me == null ||
-                    _me.ClassId != ClassId.CDOTA_Unit_Hero_Terrorblade) return;
-                _loaded = true;
+                if (!Game.IsInGame || Game.IsWatchingGame || me == null ||
+                    me.ClassId != ClassId.CDOTA_Unit_Hero_Terrorblade) return;
+                loaded = true;
 
             }
             if (!Utils.SleepCheck("toggle") || Game.IsChatOpen || Game.IsPaused ||
@@ -53,21 +53,21 @@ namespace TerrorAutoUlti
         {
             if (!Game.IsInGame || Game.IsPaused || Game.IsWatchingGame || !Menu.Item("enabled").GetValue<bool>())
                 return;
-            _me = ObjectManager.LocalHero;
-            if (_me == null)
+            me = ObjectManager.LocalHero;
+            if (me == null)
                 return;
-            _enemies = ObjectManager.GetEntitiesParallel<Hero>().Where(x => _me.Team != x.Team && x.IsValid && !x.IsIllusion && x.IsAlive).ToList();
+            _enemies = ObjectManager.GetEntitiesParallel<Hero>().Where(x => me.Team != x.Team && x.IsValid && !x.IsIllusion && x.IsAlive).ToList();
             if (_enemies == null)
                 return;
 
-            var ulti = _me.Spellbook.SpellR;
-           // var ultiLevel = _me.Spellbook.SpellR.Level - 1;
+            var ulti = me.Spellbook.SpellR;
+           // var ultiLevel = me.Spellbook.SpellR.Level - 1;
 
             foreach (var v in _enemies)
             {
-                if (!_me.IsValid || !v.IsValid || !(_me.Distance2D(v) <= ulti.GetCastRange()) || !ulti.CanBeCasted() ||
-                    !_me.CanCast() || !Utils.SleepCheck("1")) continue;
-                if (_me.Health / _me.MaximumHealth * 100 >= Menu.Item("slider").GetValue<Slider>().Value)
+                if (!me.IsValid || !v.IsValid || !(me.Distance2D(v) <= ulti.GetCastRange()) || !ulti.CanBeCasted() ||
+                    !me.CanCast() || !Utils.SleepCheck("1")) continue;
+                if (me.Health / me.MaximumHealth * 100 >= Menu.Item("slider").GetValue<Slider>().Value)
                     continue;
                 ulti.UseAbility(v);
                 Utils.Sleep(1000, "1");
