e27874ce7e55360b2471dd011d3fa6c4f52e3f44
BlackSharp <makcum31ru@gmail.com>
2017-04-24 22:44:16 +0300

    fix

--- TerrorAutoUltiAfterfix/Terror.cs
+++ TerrorAutoUltiAfterfix/Terror.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using Ensage;
 using Ensage.Common;
+using Ensage.Common.Extensions;
 using Ensage.Common.Menu;
 
 namespace TerrorAutoUlti
@@ -9,7 +10,6 @@ namespace TerrorAutoUlti
     internal class TerrorAutoUlti
     {
         private static readonly Menu Menu = new Menu("TerrorAutoUlti", "TerrorAutoUlti by Black", true, "npc_dota_hero_terrorblade", true);
-        private static bool _active = true;
         private static Hero _me;
         private static System.Collections.Generic.List<Hero> _enemies;
         private static bool _loaded;
@@ -17,8 +17,8 @@ namespace TerrorAutoUlti
         {
             if (ObjectManager.LocalHero.ClassId != ClassId.CDOTA_Unit_Hero_Terrorblade) return;
             Menu.AddItem(new MenuItem("enabled", "Enabled").SetValue(true));
-            Menu.AddItem(new MenuItem("Key", "Combo key").SetValue(new KeyBind('D', KeyBindType.Toggle)));
-            Menu.AddItem(new MenuItem("Heel", "Min hp to swap").SetValue(new Slider(2, 1, 5)));
+            Menu.AddItem(new MenuItem("toggle", "Toggle").SetValue(new KeyBind('G', KeyBindType.Toggle)));
+            Menu.AddItem(new MenuItem("slider", "Min hp % to swap").SetValue(new Slider(25, 5, 75)));
             Menu.AddToMainMenu();
             Game.OnUpdate += Game_OnUpdate;
         }
@@ -40,49 +40,35 @@ namespace TerrorAutoUlti
 
                 if (!Game.IsInGame || Game.IsWatchingGame || _me == null ||
                     _me.ClassId != ClassId.CDOTA_Unit_Hero_Terrorblade) return;
-
-                if (!Menu.Item("enabled").IsActive())
-                    return;
                 _loaded = true;
-                _active = true;
 
             }
-            if (!Game.FindKeyValues() || !Utils.SleepCheck("toggle") || Game.IsChatOpen || Game.IsPaused ||
+            if (!Utils.SleepCheck("toggle") || Game.IsChatOpen || Game.IsPaused ||
                 Game.IsWatchingGame) return;
             Utils.Sleep(200, "toggle");
         }
         public static void Tick(EventArgs args)
         {
-            if (!Game.IsInGame || Game.IsPaused || Game.IsWatchingGame)
+            if (!Game.IsInGame || Game.IsPaused || Game.IsWatchingGame || !Menu.Item("enabled").GetValue<bool>())
                 return;
             _me = ObjectManager.LocalHero;
             if (_me == null)
                 return;
-            _enemies = ObjectManager.GetEntities<Hero>().Where(x => _me.Team != x.Team && !x.IsIllusion && x.IsAlive).ToList();
+            _enemies = ObjectManager.GetEntitiesParallel<Hero>().Where(x => _me.Team != x.Team && x.IsValid && !x.IsIllusion && x.IsAlive).ToList();
             if (_enemies == null)
                 return;
 
+            var ulti = _me.Spellbook.SpellR;
+           // var ultiLevel = _me.Spellbook.SpellR.Level - 1;
+
             foreach (var v in _enemies)
             {
-                if (!_active) continue;
-                if (_me.Player.Hero.Level < 11  && Utils.SleepCheck("1") && _me.Player.Hero.IsAlive && _me.ClassId == ClassId.CDOTA_Unit_Hero_Terrorblade)
-                {
-                    if (_me.Player.Hero.Health < 200)
-                    {
-                        if (v.Player.Hero.Health > _me.Player.Hero.Health)
-                        {
-                            _me.Spellbook.SpellR.UseAbility(v.Player.Hero);
-                            Utils.Sleep(1000, "1");
-                        }
-                    }
-
-                }
-                if (_me.Player.Hero.Level <= 11 || !Utils.SleepCheck("2") || !_me.Player.Hero.IsAlive ||
-                    _me.ClassId != ClassId.CDOTA_Unit_Hero_Terrorblade) continue;
-                if (_me.Player.Hero.Health >= 400) continue;
-                if (v.Player.Hero.Health <= _me.Player.Hero.Health) continue;
-                _me.Spellbook.SpellR.UseAbility(v.Player.Hero);
-                Utils.Sleep(1000, "2");
+                if (!_me.IsValid || !v.IsValid || !(_me.Distance2D(v) <= ulti.GetCastRange()) || !ulti.CanBeCasted() ||
+                    !_me.CanCast() || !Utils.SleepCheck("1")) continue;
+                if ((_me.Health / _me.MaximumHealth) * 100 >= Menu.Item("slider").GetValue<Slider>().Value)
+                    continue;
+                ulti.UseAbility(v);
+                Utils.Sleep(1000, "1");
             }
         }
     }
