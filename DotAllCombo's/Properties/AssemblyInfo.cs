1fe169363fd95b9a4f4395e9302cc3801bf54a6c
BlackSharp <makcum31ru@gmail.com>
2017-04-26 14:28:25 +0300

    upd

--- DotAllCombo's/Properties/AssemblyInfo.cs
+++ DotAllCombo's/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("DotaAllCombo's")]
+[assembly: AssemblyTitle("DotaAllCombo's by Vick (Reworked by Black)")]
 [assembly: AssemblyDescription("A multihero multimodule assembly for Ensage (Dota 2 .NET API)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
